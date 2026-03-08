869b1763e8f37079447f3f0ec8967410076a1b96
BryanBDS <brayansrz.2018@gmail.com>
2026-03-08 13:44:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -194,6 +194,21 @@ document.getElementById("pantalla").innerHTML = `
 }
 
 
+function switchTab(tab){
+
+document.querySelectorAll(".content-tab").forEach(section=>{
+section.classList.remove("active");
+});
+
+document.getElementById("tab-"+tab).classList.add("active");
+
+document.querySelectorAll(".nav-btn").forEach(btn=>{
+btn.classList.remove("active");
+});
+
+event.target.classList.add("active");
+
+}
 
 
 
