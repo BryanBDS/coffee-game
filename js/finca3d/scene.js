79ee6130344bee06696598c55c081fdea70fbbb7
BryanBDS <brayansrz.2018@gmail.com>
2026-03-15 16:58:07 -0500

    Actualizar scene.js

--- js/finca3d/scene.js
+++ js/finca3d/scene.js
@@ -44,16 +44,41 @@ const light = new THREE.DirectionalLight(0xffffff,1);
 light.position.set(5,10,5);
 scene.add(light);
 
-/* TERRENO VERDE */
+/* TERRENO BASE */
 
-const groundGeo = new THREE.PlaneGeometry(20,20);
-const groundMat = new THREE.MeshStandardMaterial({color:0x2e7d32});
+const groundGeo = new THREE.PlaneGeometry(40,40);
+const groundMat = new THREE.MeshStandardMaterial({color:0x4caf50});
 
 const ground = new THREE.Mesh(groundGeo,groundMat);
-
 ground.rotation.x = -Math.PI/2;
 
 scene.add(ground);
+
+
+/* CREAR PARCELAS DE CULTIVO */
+
+const parcelas = [];
+
+const parcelaGeo = new THREE.PlaneGeometry(3,3);
+const parcelaMat = new THREE.MeshStandardMaterial({color:0x6d4c41});
+
+for(let x=-12; x<=12; x+=4){
+
+for(let z=-12; z<=12; z+=4){
+
+const parcela = new THREE.Mesh(parcelaGeo, parcelaMat);
+
+parcela.rotation.x = -Math.PI/2;
+
+parcela.position.set(x,0.01,z);
+
+scene.add(parcela);
+
+parcelas.push(parcela);
+
+}
+
+}
 
 /* CUBO ROJO */
 
