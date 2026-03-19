cd838f6896bd93e3e4be824d8040869b4f8ae9ba
BryanBDS <brayansrz.2018@gmail.com>
2026-03-18 19:32:23 -0500

    Actualizar finca.js

--- js/modules/finca.js
+++ js/modules/finca.js
@@ -1,17 +1,5 @@
-
-let plantas = 0
-let cafeListo = false
-
-
-window.mostrarMapa = function(){
-
-console.log("Mapa activado");
-
-/* SOLO cambiar de pestaña */
-switchTab("abrirMapa");
-
-}
-
+let plantas = 0;
+let cafeListo = false;
 
 window.comprarTerreno = function(){
 
@@ -29,97 +17,15 @@ GameManager.money -= 500;
 const id = Date.now();
 
 GameManager.comprarParcela(id);
-
 GameManager.guardar();
 
 /* ir a finca */
 switchTab("abrirFinca");
 
+setTimeout(()=>{
+if(window.iniciarFinca3D){
+window.iniciarFinca3D();
 }
-
-
-
-window.mostrarMapa = function(){
-
-console.log("Mapa activado");
-
-/* abrir pestaña correctamente */
-switchTab("abrirMapa");
+},300);
 
 }
-
-
-function sembrar(){
-
-plantas = 100
-
-document.getElementById("pantalla").innerHTML = `
-<h2>Café sembrado</h2>
-<p>Plantas: ${plantas}</p>
-<button onclick="mostrarFinca()">Volver</button>
-`
-
-}
-
-function crecer(){
-
-cafeListo = true
-
-document.getElementById("pantalla").innerHTML = `
-<h2>🍒 Café listo para cosechar</h2>
-<button onclick="cosechar()">Cosechar</button>
-`
-
-}
-
-function cosechar(){
-
-dinero += 800
-
-document.getElementById("pantalla").innerHTML = `
-<h2>Cosecha vendida</h2>
-<p>Ganancia: 800</p>
-<p>Dinero total: ${dinero}</p>
-<button onclick="mostrarFinca()">Volver a finca</button>
-`
-
-}
-
-function mostrarMercado(){
-
-document.getElementById("pantalla").innerHTML = `
-<h2>💰 Mercado del café</h2>
-<p>Precio actual: 2.5 CCF/kg</p>
-`
-}
-
-
-function mostrarMiFinca(){
-
-const data = localStorage.getItem("miFinca");
-
-if(!data) return;
-
-const finca = JSON.parse(data);
-
-let html = `
-<div class="card">
-
-<h2>☕ Mi finca</h2>
-
-<p><b>Municipio:</b> ${finca.municipio}</p>
-
-<p><b>Lote:</b> ${finca.lote}</p>
-
-<p><b>Precio:</b> ${finca.precio} CoffeeCoins</p>
-
-<button class="nav-btn">🌱 Sembrar café</button>
-
-</div>
-`;
-
-document.querySelector(".finca-detail").innerHTML = html;
-
-}
-
-
