1e7dec44c2f406f2454a1e91ea83c15b08fa32df
BryanBDS <brayansrz.2018@gmail.com>
2026-03-19 16:51:01 -0500

    Actualizar scene.js

--- js/three/scene.js
+++ js/three/scene.js
@@ -76,19 +76,31 @@ if(window.GameManager){
 
 console.log("Parcelas del jugador:", GameManager.parcelas);
 
-/* recorrer parcelas */
-
 GameManager.parcelas.forEach((p, index)=>{
 
-const geo = new THREE.PlaneGeometry(3,3);
-const mat = new THREE.MeshStandardMaterial({color:0x6d4c41});
+/* tamaño según hectáreas */
+let size = 3;
+
+if(p.hectareas === 2) size = 5;
+if(p.hectareas === 5) size = 8;
+
+/* geometría */
+const geo = new THREE.PlaneGeometry(size, size);
+
+/* color según estado */
+let color = 0x6d4c41; // café
+
+if(p.estado === "sembrado") color = 0x2e7d32;
+if(p.estado === "listo") color = 0xffeb3b;
+
+const mat = new THREE.MeshStandardMaterial({color: color});
 
 const parcela = new THREE.Mesh(geo, mat);
 
 parcela.rotation.x = -Math.PI/2;
 
-/* distribución en fila */
-parcela.position.set(index * 4, 0.02, 0);
+/* posición separada */
+parcela.position.set(index * (size + 1), 0.02, 0);
 
 scene.add(parcela);
 
